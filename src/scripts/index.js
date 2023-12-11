3947e51b0c7950efbfb65ca2125057caa545d440
Mcorduk <om.corduk@gmail.com>
2023-12-11 17:03:24 +0300

    Dark Light Theme IIFE, Dialog box and a form inside to add todo's, functionality to dialog box

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,34 @@ const outputBox = document.querySelector("output");
 const selectEl = favDialog.querySelector("select");
 const confirmBtn = favDialog.querySelector("#confirmBtn");
 
+
+
+function validateForm() {
+    var title = document.getElementById('title').value;
+    var dueDate = document.getElementById('birthday').value;
+    var validationMessageElement = document.getElementById('titleValidationMessage');
+    var dateValidationMessageElement = document.getElementById('dateValidationMessage');
+
+    // Clear previous validation messages
+    validationMessageElement.innerHTML = '';
+
+    if (title.trim() === '') {
+        validationMessageElement.innerHTML = 'Todo Title cannot be empty.';
+        return false;
+    }
+
+    if (dueDate.trim() === '') {
+        dateValidationMessageElement.innerHTML = 'Due Date cannot be empty.';
+        return false;
+    }
+
+    // Additional validation logic can be added here
+
+    return true; // If all validation passes, the form will be submitted
+}
+
+// DIALOG BUTTON FUNCTIONALITY
+(function dialogListeners(){
 // "Show the dialog" button opens the <dialog> modally
 showButton.addEventListener("click", () => {
   favDialog.showModal();
@@ -26,6 +54,43 @@ favDialog.addEventListener("close", (e) => {
 
 // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+        
+        event.preventDefault(); // We don't want to submit this fake form
+        favDialog.close(selectEl.value); // Have to send the select box value here.
+    
 });
+})();
+
+//DARK/LIGHT THEME TOGGLE IIFE
+(function themeToggle() {
+  // TOGGLE THEME
+  // Get the theme toggle button element
+  const themeToggle = document.getElementById('theme-toggle');
+
+  // Function to toggle the dark theme
+  function toggleDarkTheme() {
+    document.body.classList.toggle('dark-theme');
+
+    // Toggle the dark theme class on the dialog element
+    const dialogElement = document.querySelector('dialog');
+    if (dialogElement) {
+      dialogElement.classList.toggle('dark');
+    }
+
+    // Save the user's theme preference to Local Storage
+    const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('theme', currentTheme);
+  }
+
+  // Event listener for the theme toggle button
+  themeToggle.addEventListener('click', toggleDarkTheme);
+
+  // Check if the user's theme preference is already set
+  const userThemePreference = localStorage.getItem('theme');
+
+  // If the preference is set, apply the theme
+  if (userThemePreference === 'dark') {
+    document.body.classList.add('dark-theme');
+  }
+})();
+
