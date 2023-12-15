d5ccd3fea879c5926232b0742de0d11e5d3d9c7a
Mcorduk <om.corduk@gmail.com>
2023-12-15 16:27:20 +0300

    submit form returns a todo as object

--- src/scripts/dialog.js
+++ src/scripts/dialog.js
@@ -1,23 +1,54 @@
-function dialogListeners() {
-    const showButton = document.getElementById("showDialog");
+import { createTodo } from "./todos";
+import function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const form = event.target; // Get the form that triggered the submit event
+    const formData = new FormData(form);
+
+    // Convert FormData to a plain object
+    const formObject = {};
+    formData.forEach((value, key) => {
+        formObject[key] = value;
+    });
+
+    // Create a todo object using the form data
+    const todo = createTodo(
+        formObject.title,
+        formObject.description,
+        formObject.dueDate,
+        formObject.priority
+    );
+
+    // Log the todo object to the console
+    console.log(todo);
+    console.log(todo.getTitle());
+    // Close the dialog if needed
     const favDialog = document.getElementById("favDialog");
-    const confirmBtn = favDialog.querySelector("#confirmBtn");
-
-
-    // "Show the dialog" button opens the <dialog> modally
-    const showDialog = () => {
-        showButton.addEventListener("click", () => {
-            favDialog.showModal();
-        })
+    if (favDialog) {
+        favDialog.close();
     }
 
-    // Prevent the "confirm" button from the default behavior of submitting form,
-    //and close the dialog with the `close()` method, triggers "close" event.
-    
-    //Show dialog box via button
-    showDialog()
+
+};
+
+function showDialog() {
+    const showButton = document.getElementById("showDialog");
+    const favDialog = document.getElementById("favDialog");
+
+    // "Show the dialog" button opens the <dialog> modally
+    showButton.addEventListener("click", () => {
+        favDialog.showModal();
+    });
 }
 
+function dialogListeners() {
+    const form = document.getElementById("todoForm");
+
+    // Event listener for submit button
+    form.addEventListener('submit', submitForm);
+
+    // Show dialog box via button
+    showDialog();
+}
 
 export { dialogListeners };
-
